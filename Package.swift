e28408ad6b591f8073b093caf649f01c4028d373
simyo <hj.sim@muzlive.com>
2023-08-18 16:49:28 +0900

    fix: products, targets 섹션 수정

--- Package.swift
+++ Package.swift
@@ -9,15 +9,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TheAmazingAudioEngineSPM",
-            targets: ["TheAmazingAudioEngineSPM"]),
+            targets: ["TheAmazingAudioEngine"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TheAmazingAudioEngineSPM"),
-        .testTarget(
-            name: "TheAmazingAudioEngineSPMTests",
-            dependencies: ["TheAmazingAudioEngineSPM"]),
+            name: "TheAmazingAudioEngine",
+            dependencies: []),
     ]
 )
